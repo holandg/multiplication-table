e79785868a32bb0ff3c0e59a4d8cb1126b02086f
Gabriel Filipi de Holanda Monteiro <gabriel.holanda@saude.local>
2025-03-14 14:44:59 -0300

    end commit

---
+++ app/src/main/java/com/example/multiplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.multiplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText editText;
+    Button button;
+    TextView result;
+    int ans = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        result = findViewById(R.id.textView);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button) {
+            StringBuffer buffer = new StringBuffer();
+
+            String fs = editText.getText().toString();
+
+            int n = Integer.parseInt(fs);
+
+            for (int i = 1; i <= 10; i++) {
+                ans = (i * n);
+                buffer.append(n + " X " + i + " = " + ans + "\n\n");
+            }
+            result.setText(buffer);
+        }
+    }
+}
+
+// end commit
